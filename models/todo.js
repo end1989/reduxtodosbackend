77f89929b12eb3d0fde59a95e4013eaffdf47aad
Eldon Dahlin <34949137+end1989@users.noreply.github.com>
2018-08-09 18:55:53 -0600

    Update todo.js

--- models/todo.js
+++ models/todo.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.connect(
-    "mongodb://end1989:whatever1@ds119052.mlab.com:19052/todolist"
+// 
 );
 mongoose.set("debug", true);
 mongoose.Promise = Promise;
